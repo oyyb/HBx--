49e7c88613b9d6a472440dc25c88ef20ac7b7aa1
欧阳运柄 <1694108096@qq.com>
2021-01-09 20:58:20 +0800

    订餐系统更新

---
+++ ordering_system/src/components/Customer/Main_Customer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    顾客主页
+  </div>
+</template>
+
+<script>
+export default {
+name: "Main_Customer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ordering_system/src/components/Merchant/Main_Merchant.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    商家主页
+  </div>
+</template>
+
+<script>
+export default {
+name: "Main_Merchant"
+}
+</script>
+
+<style scoped>
+
+</style>
